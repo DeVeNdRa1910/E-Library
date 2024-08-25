b0bd00a66fa20730164698677c82f308b318a2da
Devendra <devendrav1910@gmail.com>
2024-08-25 22:45:12 +0530

    created loggin end point

--- Backend/src/controllers/user/userSignInController.ts
+++ Backend/src/controllers/user/userSignInController.ts
@@ -13,22 +13,38 @@ export async function loginUser(
   try {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+      const error = createHttpError(400, "Both fields are mendatory");
+      return next(error);
+    }
+
     const user = await userModel.findOne({ email });
 
     if (!user) {
-      return next(createHttpError(401, "Email does not exist"));
+      return next(createHttpError(404, "User not found..."));
     }
 
-    const isPasswordSame = await bcrypt.compare(password, user.password);
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
 
-    if (!isPasswordSame) {
-      return next(createHttpError(401, "Wrong credentials"));
+    if (!isPasswordMatch) {
+      return next(createHttpError(400, "Wrong credentials"));
     }
 
     const tokenData = {
       id: user._id,
       email: user.email,
     };
+
+    const accessToken = jwt.sign(tokenData, config.jwtSecretKey as string, {'expiresIn': '1d'})
+
+    res.status(201).json({
+      data: user,
+      accessToken: accessToken,
+      message: "User logged in successfully",
+      error: false,
+      success: true,
+    });
+
   } catch (error: any) {
     const errorObj = {
       message: error.message || error,
