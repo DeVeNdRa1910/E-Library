585d2a16dca5ef1e3d8261c90059881ee389711d
Devendra <devendrav1910@gmail.com>
2024-09-22 20:35:20 +0530

    impliment new frontend url

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json())  // post(or get) all data from DB
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 })) // allow all browser to use this resource , you have to put frontend URL
 //store cooki on client side
