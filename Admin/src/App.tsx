a1caf6102e0d6f7c1bfd903ab279e3df148d6dd0
Devendra <devendrav1910@gmail.com>
2024-09-29 17:14:07 +0530

    test

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-
+import { Button } from './components/ui/button'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='text-red-400'>{count}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="m-5 p-5">
+        <Button className='m-3' onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
+        </Button>
+        <Button onClick={() => setCount((count) => count - 1)}>
           count is {count}
-        </button>
+        </Button>
       </div>
     </>
   )
