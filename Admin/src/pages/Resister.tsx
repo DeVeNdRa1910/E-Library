e6eb599e958953575b86bb3cbaec539274c96b12
Devendra <devendrav1910@gmail.com>
2024-09-29 18:25:32 +0530

    create static home Page

--- Admin/src/pages/Resister.tsx
+++ Admin/src/pages/Resister.tsx
@@ -11,7 +11,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 
-function Login() {
+function Resister() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Card className="w-full max-w-sm">
@@ -23,6 +23,10 @@ function Login() {
         </CardHeader>
         <CardContent className="grid gap-4">
           <div className="grid gap-2">
+            <Label htmlFor="email">Name</Label>
+            <Input id="email" type="email" placeholder="example abc" required />
+          </div>
+          <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input id="email" type="email" placeholder="m@example.com" required />
           </div>
@@ -30,19 +34,23 @@ function Login() {
             <Label htmlFor="password">Password</Label>
             <Input id="password" type="password" required />
           </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Confirm Password</Label>
+            <Input id="password" type="password" required />
+          </div>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">Login</Button>
+          <Button className="w-full">Resister</Button>
         </CardFooter>
         <div className="my-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link to={'/resister'} className="underline">
-            Sign up
+          Already have an account?{" "}
+          <Link to={'/login'} className="underline">
+            Login
           </Link>
         </div>
       </Card>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Resister
